5610f5995a80d152a4320cfa0f43fb467cde7240
jesusmsr <jsr20xx@gmail.com>
2024-07-12 23:37:21 +0200

    Close google pop up

--- screenshot.py
+++ screenshot.py
@@ -9,9 +9,36 @@ screenshotDir = "Screenshots"
 screenWidth = 400
 screenHeight = 800
 
+def __wait_for_page_load(driver, timeout=30):
+    try:
+        WebDriverWait(driver, timeout).until(
+            lambda d: d.execute_script('return document.readyState') == 'complete'
+        )
+        print("Page has fully loaded.")
+    except Exception as e:
+        print("Page did not load within the given time.", e)
+
+
+def __check_and_click_close(driver, wait):
+    try:
+        iframe_parent = driver.find_element(By.ID, 'credential_picker_container')
+        iframe = iframe_parent.find_element(By.TAG_NAME, 'iframe')
+        
+        driver.switch_to.frame(iframe)
+        
+        close_button = wait.until(EC.presence_of_element_located((By.ID, 'close')))
+        wait.until(EC.element_to_be_clickable((By.ID, 'close'))).click()
+        print("Clicked the 'close' button.")
+        
+        driver.switch_to.default_content()
+    except Exception as e:
+        print("No 'accept cookies' button found or it could not be clicked.", e)
+
 def getPostScreenshots(filePrefix, script):
     print("Taking screenshots...")
     driver, wait = __setupDriver(script.url)
+    __wait_for_page_load(driver)
+    __check_and_click_close(driver, wait)
     script.titleSCFile = __takeScreenshot(filePrefix, driver, wait, f"t3_{script.postId}", By.ID)
     for commentFrame in script.frames:
         commentFrame.screenShotFile = __takeScreenshot(filePrefix, driver, wait, f"[thingid=t1_{commentFrame.commentId}]", By.CSS_SELECTOR)
